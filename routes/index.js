2f1e37240f10b564e9f2897d17917b9cbb0dfec8
Kebba Demba <kebbademba@Kebbas-MacBook-Pro.local>
2018-11-26 23:35:00 -0600

    fix the passport

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const passport = require("Passport");
+const passport = require("passport");
 const User = require("../models/user");
 const {isLoggedIn, isTeacherLoggedIn} = require("../middleware")
 const async = require("async"),
